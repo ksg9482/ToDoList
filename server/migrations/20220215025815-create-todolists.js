119460d91dfe1e7f177b8f1f6dc13a7f4401b340
ksg9482 <ksg930523@gmail.com>
2022-02-15 23:50:19 +0900

    database

---
+++ server/migrations/20220215025815-create-todolists.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('todolists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      list: {
+        type: Sequelize.STRING
+      },
+      todostatus: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('todolists');
+  }
+};
---
+++ server/models/todolists.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class todolists extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  //생성날짜, 수정날짜, 리스트 아이디?
+  //todostatus 1:할일, 2:하고있는일, 3:한일
+  todolists.init({
+    user_id: DataTypes.INTEGER,
+    list: DataTypes.STRING,
+    todostatus:DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'todolists',
+  });
+  return todolists;
+};
