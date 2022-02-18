a26904340f2b4f1c096af1435cb9dcb478b8f890
ksg9482 <ksg930523@gmail.com>
2022-02-18 23:59:35 +0900

    auth

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -2,32 +2,45 @@
 const user = require('../models')
 
 module.exports = {
-    signup: async (req:any, res:any) => {
-    const {name, password, mail, social} = req.body
-    //password 암호화 필요
-    await user.create({
-        name: name,
-        password: password,
-        mail: mail,
-        social: social
-    })
-        res.status(201).json('userInfo')
-        return ;
+    signup: async (req: any, res: any) => {
+        const { name, password, mail, social } = req.body
+        //password 암호화 필요
+        try {
+            await user.create({
+                name: name,
+                password: password,
+                mail: mail,
+                social: social
+            })
+            return res.status(201).json('userInfo')
+
+        } catch (error) {
+            return res.status(500).json({'message': 'server error'})
+        }
+        
     },
 
-    signout: async (req:any, res:any) => {
+    signout: async (req: any, res: any) => {
+        try {
+            await user.destroy({
+                where: {id: req.userId}
+            })
+            return res.status(200).clearCookie('jwt').json({"message" : 'deleted'})  
+        } catch (error) {
+            return res.status(500).json({'message': 'server error'})
+        }
         res.json('userInfo')
     },
 
-    login: async (req:any, res:any) => {
+    login: async (req: any, res: any) => {
         res.json('userInfo')
     },
 
-    logout: async (req:any, res:any) => {
+    logout: async (req: any, res: any) => {
         res.json('userInfo')
     },
 
-    OAuth: async (req:any, res:any) => {
+    OAuth: async (req: any, res: any) => {
         res.json('userInfo')
     },
 }
