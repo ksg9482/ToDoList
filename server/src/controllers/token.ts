c32d3179594028a0a4fab712703d747b9cccdaee
ksg9482 <ksg930523@gmail.com>
2022-02-19 23:58:19 +0900

    token

--- server/src/controllers/token.ts
+++ server/src/controllers/token.ts
@@ -1,3 +1,12 @@
 require('dotenv').config()
-const {sign} = require('jsonwebtoken')
+const { sign } = require('jsonwebtoken')
 const DOMAIN = process.env.DOMAIN || 'localhost'
+
+module.exports = {
+    generateAccessToken: (data:any) => {
+        return sign(data, process.env.ACCESS_SECRET, {expireIn:'30m'})
+    },
+    sendAccessToken: () => {},
+    isAuthorized: () => {},
+    refreshToken: () => {}
+}
